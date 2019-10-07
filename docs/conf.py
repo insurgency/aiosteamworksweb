ef31eec347351c089d63dbf0634ca65603135e2e
h1nk <12993584+h1nk@users.noreply.github.com>
2019-10-07 03:44:53 -0400

    Install Sphinx and perform sphinx-quickstart

---
+++ docs/conf.py
@@ -0,0 +1,10 @@
+project = 'aiosteamworksweb'
+# noinspection PyShadowingBuiltins
+copyright = '2019, insurgency.gg'
+author = 'insurgency.gg'
+extensions = [
+]
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+html_theme = 'alabaster'
+html_static_path = ['_static']
---
+++ docs/index.rst
@@ -0,0 +1,15 @@
+Welcome to aiosteamworksweb's documentation!
+============================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
