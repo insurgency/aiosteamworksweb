07cf2da05fd3b778e0c29457592ea44d4bb484ca
h1nk <12993584+h1nk@users.noreply.github.com>
2019-10-07 03:41:02 -0400

    Create boilerplate setuptools script and install aiohttp package

---
+++ setup.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python3
+
+from setuptools import setup, find_packages
+
+setup(
+    name='aiosteamworksweb',
+    version=None,  # TODO
+    author='insurgency.gg',
+    url='https://github.com/insurgency/aiosteamworksweb',
+    packages=find_packages(
+        exclude=[
+            'tests',
+        ],
+    ),
+    classifiers=[
+    ],
+    license='MIT',
+    python_requires='>=3.7',
+    test_suite='tests',
+    install_requires=[
+        'aiohttp >= 3.6.1',
+    ],
+)
