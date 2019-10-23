a8e6b82850862a2219e8e8f5c8617a67a0c4c144
h1nk <12993584+h1nk@users.noreply.github.com>
2019-10-23 16:03:28 -0400

    Install Sphinx autodoc & autodoc-typehints extensions

--- docs/conf.py
+++ docs/conf.py
@@ -3,6 +3,9 @@ project = 'aiosteamworksweb'
 copyright = '2019, insurgency.gg'
 author = 'insurgency.gg'
 extensions = [
+    'sphinx.ext.autodoc',
+    # See: https://github.com/agronholm/sphinx-autodoc-typehints#installation-and-setup
+    'sphinx_autodoc_typehints',
 ]
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
