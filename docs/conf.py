ca8bdd32665b0ded3953fe992c0664049d247228
h1nk <12993584+h1nk@users.noreply.github.com>
2019-10-23 17:14:47 -0400

    Install intersphinx extension

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,5 @@
+import sys
+
 project = 'aiosteamworksweb'
 # noinspection PyShadowingBuiltins
 copyright = '2019, insurgency.gg'
@@ -6,8 +8,12 @@ extensions = [
     'sphinx.ext.autodoc',
     # See: https://github.com/agronholm/sphinx-autodoc-typehints#installation-and-setup
     'sphinx_autodoc_typehints',
+    'sphinx.ext.intersphinx',
 ]
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 html_theme = 'alabaster'
 html_static_path = ['_static']
+intersphinx_mapping = {
+    'py': ('https://docs.python.org/{0.major}.{0.minor}'.format(sys.version_info), None),
+}
