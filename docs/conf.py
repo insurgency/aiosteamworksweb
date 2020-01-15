e26c6d6b402f77baf5a66a12c265da0024c697fc
h1nk <12993584+h1nk@users.noreply.github.com>
2020-01-15 14:05:18 -0500

    Update Sphinx config
    The Sphinx config for this project will now pull commonly shared config
    values from the central organization Sphinx docs config. This will help
    keep stuff more DRY and reduce the amount of repeated boilerplate
    between our different projects that are documented with Sphinx.

--- docs/conf.py
+++ docs/conf.py
@@ -1,30 +1,4 @@
-import sys
+# noinspection PyPackageRequirements,PyUnresolvedReferences
+from documentation.conf import *
 
 project = 'aiosteamworksweb'
-# noinspection PyShadowingBuiltins
-copyright = '2019, insurgency.gg'
-author = 'insurgency.gg'
-extensions = [
-    'sphinx.ext.autodoc',
-    # See: https://github.com/agronholm/sphinx-autodoc-typehints#installation-and-setup
-    'sphinx_autodoc_typehints',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.extlinks',
-]
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-html_theme = 'alabaster'
-html_static_path = ['_static']
-intersphinx_mapping = {
-    'py': ('https://docs.python.org/{0.major}.{0.minor}'.format(sys.version_info), None),
-    'aiohttp': ('https://docs.aiohttp.org/en/stable/', None),
-    'yarl': ('https://yarl.rtfd.io/en/stable/', None),
-}
-autodoc_member_order = 'bysource'
-extlinks = {
-    'github-branch': (f'https://github.com/insurgency/{project}/tree/branches/%s', 'name'),
-    'issue': (f'https://github.com/insurgency/{project}/issues/%s', 'gh-'),
-    'valve-wiki': ('https://developer.valvesoftware.com/wiki/%s', 'page'),
-    'wikipedia': ('https://wikipedia.org/wiki/%s', 'page'),
-    'steam-app': ('https://store.steampowered.com/app/%s', 'app-id')
-}
